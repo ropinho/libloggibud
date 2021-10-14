4d3120efe359f695403cc5288d602424c816fa69
Ronaldd Pinho <ronaldd.pinho@lambda3.com.br>
2021-10-14 13:23:31 -0300

    add types.h

---
+++ include/loggibud/types.h
@@ -0,0 +1,11 @@
+#ifndef LOGGIBUD_CXX_TYPE_H_
+#define LOGGIBUD_CXX_TYPE_H_
+
+#include "point.h"
+#include "delivery.h"
+#include "delivery_problem_instance.h"
+#include "cvrp_instance.h"
+#include "cvrp_solution_vehicle.h"
+#include "cvrp_solution.h"
+
+#endif
