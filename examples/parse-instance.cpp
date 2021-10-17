cc4d67ce6afd823f28fb0242224ffb8b2ba425ee
Ronaldd Pinho <ronaldd.pinho@lambda3.com.br>
2021-10-17 13:00:27 -0300

    update example parse-instance

--- examples/parse-instance.cpp
+++ examples/parse-instance.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char const *argv[]) {
     if (argc < 2) {
-        std::cerr << "Use " << argv[1] << " [path/to/instance.json]\n";
+        std::cerr << "Use " << argv[0] << " [path/to/instance.json]" << std::endl;
         return EXIT_FAILURE;
     }
 
@@ -13,7 +13,8 @@ int main(int argc, char const *argv[]) {
     try {
         loggibud::CVRPInstance problem = loggibud::CVRPInstance::from_file(filename); 
         std::cout << "Name: " << problem.name << std::endl;
-        std::cout << "Num. of deliveries: " << problem.deliveries.size() << std::endl;
+        std::cout << "Region: " << problem.region << std::endl;
+        // std::cout << "Num. of deliveries: " << problem.deliveries.size() << std::endl;
     } catch(std::exception &e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return EXIT_FAILURE;
